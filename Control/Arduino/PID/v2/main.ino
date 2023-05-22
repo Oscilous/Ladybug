d09de59aeb8a39003ba5d1eebfa4464ad23060f4
Oscilous <arfil21@student.sdu.dk>
2023-05-22 14:40:43 +0200

    Arduino code (Flash+PID)

--- Control/Arduino/PID/v2/main.ino
+++ Control/Arduino/PID/v2/main.ino
@@ -1,14 +1,3 @@
-/*
-  NanoBLEFlashPrefsTest
-
-  Demonstrates the use of the NanoBLEFlashPrefs library for storing
-  preferences and other values in flash memory
-
-  created 2021
-  by Dirk Fröhling
-
-  https://github.com/Dirk-/NanoBLEFlashPrefs
-*/
 #include <Arduino.h>
 #include <NanoBLEFlashPrefs.h>
 #include <QuickPID.h>
@@ -18,65 +7,76 @@
 #define motor_3 8
 #define motor_2 9
 #define motor_1 10
-// Access to the library
-NanoBLEFlashPrefs myFlashPrefs;
 
-// Structure of preferences. You determine the fields.
-// Must not exeed 1019 words (4076 byte).
 typedef struct flashStruct
 {
   unsigned char pwm_motor_1[ARRAY_SIZE];
   unsigned char pwm_motor_2[ARRAY_SIZE];
   unsigned char pwm_motor_3[ARRAY_SIZE];
   unsigned char pwm_motor_4[ARRAY_SIZE];
+  float output_test[ARRAY_SIZE];
 } flashPrefs;
+
+class DegreeOfFreedom
+{
+  public:
+    float setpoint = 0;
+    float input = 0;
+    float output = 0;
+    float P = 0;
+    float I = 0;
+    float D = 0;
+    DegreeOfFreedom(float p, float i, float d) : setpoint(0), input(0), output(0), P(p), I(i), D(d) {}
+    DegreeOfFreedom(float set, float p, float i, float d) : setpoint(set), input(0), output(0), P(p), I(i), D(d) {}
+};
+
+// Access to the library
+NanoBLEFlashPrefs myFlashPrefs;
 
 // Our preferences. All functions here can read and modify these values, but to make
 // them permanent, the struct must be written to flash explicitly (see below).
 flashPrefs globalPrefs;
 
+// Structure of preferences. You determine the fields.
+// Must not exeed 1019 words (4076 byte).
+
+//Constants
 const float thrust_const = 0.00004;
 const float torque_const = 0.00116;
 const float motor_distance = 0.116 / 2;
 const float pi = 3.14159;
-//Define Variables we'll be connecting to
-//Z PID
-float z_setpoint, z_input, z_output;
-float z_Kp = 0.6 * 1.7, z_Ki = 1.2*1.7/3-0.3, z_Kd = 0.075*1.7*3 - 0.1;
-QuickPID z_PID(&z_input, &z_output, &z_setpoint);
 
-//Phi PID
-float phi_setpoint, phi_input, phi_output;
-float phi_Kp = 0.6 * 1.7, phi_Ki = 1.2*1.7/3-0.3, phi_Kd = 0.075*1.7*3 - 0.1;
-QuickPID phi_PID(&phi_input, &phi_output, &phi_setpoint);
-
-//Theta PID
-float theta_setpoint, theta_input, theta_output;
-float theta_Kp = 0.6 * 1.7, theta_Ki = 1.2*1.7/3-0.3, theta_Kd = 0.075*1.7*3 - 0.1;
-QuickPID theta_PID(&theta_input, &theta_output, &theta_setpoint);
-
+//Declearing of degrees of freedom for collective variables
+//Check constructors for proper use
+DegreeOfFreedom Z(0.5, 1.02, 0.38, 0.2825);
+DegreeOfFreedom Phi(-2.29551125806385e-05, -1.42052143562565e-06, -5.90349375287184e-05);
+DegreeOfFreedom Theta(-2.34228279252575e-05, -1.44946486469699e-06, -6.02377870487898e-05);
+QuickPID Z_PID(&Z.input, &Z.output, &Z.setpoint);
+QuickPID Phi_PID(&Phi.input, &Phi.output, &Phi.setpoint);
+QuickPID Theta_PID(&Theta.input, &Theta.output, &Theta.setpoint);
 float motor_speed[4];
 
 void setup()
 {
-  // Give user a chance to open the terminal
+  // Toggle LED for indication of on/off state
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
+  //Initialize serial
   Serial.begin(9600);
   delay(3000);
-  z_setpoint = 10;
-  phi_setpoint = 0;
-  theta_setpoint = 0;
-  z_input = 0;
-  //apply PID gains
-  z_PID.SetTunings(z_Kp, z_Ki, z_Kd);
-  phi_PID.SetTunings(phi_Kp, phi_Ki, phi_Kd);
-  theta_PID.SetTunings(theta_Kp, theta_Ki, theta_Kd);
 
-  //turn the PID on
-  z_PID.SetMode(z_PID.Control::automatic);
-  phi_PID.SetMode(phi_PID.Control::automatic);
-  theta_PID.SetMode(theta_PID.Control::automatic);
+
+  //Apply PID gains
+  Z_PID.SetTunings(Z.P, Z.I, Z.D);
+  Phi_PID.SetTunings(Phi.P, Phi.I, Phi.D);
+  Theta_PID.SetTunings(Theta.P, Theta.I, Theta.D);
+
+  //Turn the PID on
+  Z_PID.SetMode(Z_PID.Control::automatic);
+  Phi_PID.SetMode(Phi_PID.Control::automatic);
+  Theta_PID.SetMode(Theta_PID.Control::automatic);
+  
+  //If the drone is connect to serial, we enter debug mode
   if (Serial)
   {
     while (1){
@@ -88,7 +88,7 @@ void setup()
       {
         Serial.print("-- Info: ");
         Serial.println(myFlashPrefs.statusString());
-        printPreferences(globalPrefs);
+        readPreferenceRecord(globalPrefs);
       }
       else if (cmdStr == "d")
       {
@@ -108,27 +108,27 @@ void setup()
       }
     }
   }
+
+  //Motors ramp up, to decrease the current spike
   motor_ramp_up();
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  while (z_input < 15){
-    z_PID.Compute(); //After this line z_output gets updated
-    z_output /= cos(theta_input) * cos(phi_input);
-    z_output /= -4 * thrust_const;
-    phi_PID.Compute();
-    phi_output /= 4 * thrust_const * sin(pi/4) * motor_distance;
-    theta_PID.Compute();
-    theta_output /= 4 * thrust_const * sin(pi/4) * motor_distance;
-
+  while (Z.input < 15){
+    Phi_PID.Compute();
+    Phi.output /= 4 * thrust_const * sin(pi/4) * motor_distance;
+    Theta_PID.Compute();
+    Theta.output /= 4 * thrust_const * sin(pi/4) * motor_distance;
+    Z_PID.Compute(); //After this line Z.output gets updated
+    globalPrefs.output_test[(int)Z.input] = Z.output;
+    Z.output /= cos(Theta.input) * cos(Phi.input);
+    Z.output /= -4 * thrust_const;
     // We calculate angular velocities
-    motor_speed[0] = z_output + phi_output + theta_output;
-    motor_speed[1] = z_output - phi_output + theta_output;
-    motor_speed[2] = z_output - phi_output - theta_output;
-    motor_speed[3] = z_output + phi_output - theta_output;
-    
+    motor_speed[0] = Z.output + Phi.output + Theta.output;
+    motor_speed[1] = Z.output - Phi.output + Theta.output;
+    motor_speed[2] = Z.output - Phi.output - Theta.output;
+    motor_speed[3] = Z.output + Phi.output - Theta.output;
     for (int i = 0; i < 4; i++){
       if (motor_speed[i] > 3500){
         motor_speed[i] = 3500;
@@ -149,18 +149,20 @@ void loop()
         motor_speed[i] = 0;
       }
     }
+
+    globalPrefs.pwm_motor_1[(int)Z.input] = motor_speed[0]; 
+    globalPrefs.pwm_motor_2[(int)Z.input] = motor_speed[1];
+    globalPrefs.pwm_motor_3[(int)Z.input] = motor_speed[2];
+    globalPrefs.pwm_motor_4[(int)Z.input] = motor_speed[3];
+
     for (int i = 0; i < 4;i++){
       analogWrite(10 - i, motor_speed[i]);
     }
-    delay(10);
-    globalPrefs.pwm_motor_1[(int)z_input] = motor_speed[0];
-    globalPrefs.pwm_motor_2[(int)z_input] = motor_speed[1];
-    globalPrefs.pwm_motor_3[(int)z_input] = motor_speed[2];
-    globalPrefs.pwm_motor_4[(int)z_input] = motor_speed[3];
-    z_input++;
+    delay(100);
+    Z.input += 0.1;
   }
   //When done send the logs
-  printReturnCode(myFlashPrefs.writePrefs(&globalPrefs, sizeof(globalPrefs)));
+  myFlashPrefs.writePrefs(&globalPrefs, sizeof(globalPrefs));
   while (1);
 }
 
@@ -184,6 +186,10 @@ void printPreferences(flashPrefs thePrefs)
   for (int i = 0; i < ARRAY_SIZE; i++){
     Serial.println(thePrefs.pwm_motor_4[i]);
   }
+  Serial.println("Z: ");
+  for (int i = 0; i < ARRAY_SIZE; i++){
+    Serial.println(thePrefs.output_test[i]);
+  }
 }
 
 // Print return code infos to Serial.
@@ -195,11 +201,11 @@ void printReturnCode(int rc)
   Serial.println(myFlashPrefs.errorString(rc));
 }
 
-void readPreferenceRecord(flashPrefs localPrefs){
-  int rc = myFlashPrefs.readPrefs(&localPrefs, sizeof(localPrefs));
+void readPreferenceRecord(flashPrefs globalPrefs){
+  int rc = myFlashPrefs.readPrefs(&globalPrefs, sizeof(globalPrefs));
   if (rc == FDS_SUCCESS)
   {
-    printPreferences(localPrefs);
+    printPreferences(globalPrefs);
   }
   else
   {

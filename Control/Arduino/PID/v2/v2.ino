73b39b24659fbe4096382c699853506ded288fe4
Oscilous <arfil21@student.sdu.dk>
2023-05-22 18:46:16 +0200

    SAVE ME

--- Control/Arduino/PID/v2/v2.ino
+++ Control/Arduino/PID/v2/v2.ino
@@ -2,8 +2,10 @@
 #include <NanoBLEFlashPrefs.h>
 #include <QuickPID.h>
 #include <LSM6DS3.h>
+#include <math.h>
+#include <Adafruit_VL53L0X.h>
 
-#define ARRAY_SIZE 15
+#define ARRAY_SIZE 20
 #define motor_4 7
 #define motor_3 8
 #define motor_2 9
@@ -15,7 +17,6 @@ typedef struct flashStruct
   unsigned char pwm_motor_2[ARRAY_SIZE];
   unsigned char pwm_motor_3[ARRAY_SIZE];
   unsigned char pwm_motor_4[ARRAY_SIZE];
-  float output_test[ARRAY_SIZE];
 } flashPrefs;
 
 class DegreeOfFreedom
@@ -30,6 +31,27 @@ class DegreeOfFreedom
     DegreeOfFreedom(float p, float i, float d) : P(p), I(i), D(d) {}
     DegreeOfFreedom(float set, float p, float i, float d) : setpoint(set), P(p), I(i), D(d) {}
 };
+
+int i=0;
+float dt=0, time_last = 0;
+float gyro_calib[3] = {-0.67,-1.47,0.5};
+float gyro[3] = {0.001, 0.001, 0.001}; // unfiltered gyro readings
+float gyro_prev[3] = {0.001, 0.001, 0.001};
+float HPF_gyro_angle[3] = {0.0, 0.0, 0.0};//FILTERED GYRO ANGLE VALUES
+float Unfilt_angle_gyro[3] = {0.0, 0.0, 0.0};//UNFILTERED GYRO ANGLE VALUES
+float Unfilt_angle_gyro_prev[3] = {0.0, 0.0, 0.0};//UNFILTERED GYRO ANGLE VALUES (previous iteration)
+
+float coefgyro_HPF[3] = {0.5968, 0.7984, -0.7984}; // HPF coefficients
+
+float acc[3] = {0.001, 0.001, 0.001};  // unfiltered acc readings
+float acc_prev[3] = {0.001, 0.001, 0.001};
+float LPF_acc_angle[3] = {0.0, 0.0, 0.0};//FILTERED ACC ANGLE VALUES
+
+float coefacc_LPF[3] = {0.5968, 0.2016, 0.2016}; // LPF coefficients
+
+float Complimentary[3] = {0.0, 0.0, 0.0};//UNFILTERED GYRO ANGLE VALUES
+
+float Z_position=0, Z_actual = 0;
 
 // Access to the library
 NanoBLEFlashPrefs myFlashPrefs;
@@ -49,14 +71,187 @@ const float pi = 3.14159;
 
 //Declearing of degrees of freedom for collective variables
 //Check constructors for proper use
-DegreeOfFreedom Z(-ARRAY_SIZE, 1.02, 0.38, 0.2825);
+DegreeOfFreedom Z(0, 1.02, 0.38, 0.2825);
 //DegreeOfFreedom Z(0.5, 1.02, 0.38, 0.2825);
 DegreeOfFreedom Phi(-2.29551125806385e-05, -1.42052143562565e-06, -5.90349375287184e-05);
 DegreeOfFreedom Theta(-2.34228279252575e-05, -1.44946486469699e-06, -6.02377870487898e-05);
 QuickPID Z_PID(&Z.input, &Z.output, &Z.setpoint);
 QuickPID Phi_PID(&Phi.input, &Phi.output, &Phi.setpoint);
 QuickPID Theta_PID(&Theta.input, &Theta.output, &Theta.setpoint);
+LSM6DS3 myIMU(I2C_MODE, 0x6A);    //I2C device address 0x6A
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();                                    // create object lox from Adafruit_VL53L0X
+VL53L0X_RangingMeasurementData_t measure; 
 float motor_speed[4];
+int j = 0;
+
+// Print preference record to Serial.
+// You have to adapt this to your particular structure.
+void printPreferences(flashPrefs thePrefs)
+{
+  for (int i = 0; i < ARRAY_SIZE; i++){
+    Serial.print(thePrefs.pwm_motor_1[i]);
+    Serial.print("  ");
+    Serial.print(thePrefs.pwm_motor_2[i]);
+    Serial.print("  ");
+    Serial.print(thePrefs.pwm_motor_3[i]);
+    Serial.print("  ");
+    Serial.println(thePrefs.pwm_motor_4[i]);
+  }
+}
+
+// Print return code infos to Serial.
+void printReturnCode(int rc)
+{
+  Serial.print("Return code: ");
+  Serial.print(rc);
+  Serial.print(", ");
+  Serial.println(myFlashPrefs.errorString(rc));
+}
+
+void readPreferenceRecord(flashPrefs globalPrefs){
+  int rc = myFlashPrefs.readPrefs(&globalPrefs, sizeof(globalPrefs));
+  if (rc == FDS_SUCCESS)
+  {
+    printPreferences(globalPrefs);
+  }
+  else
+  {
+    printReturnCode(rc);
+  }
+}
+
+void motor_ramp_up(){
+  for (int i = 0; i < 200;i++){
+    analogWrite(10, i);
+    analogWrite(9, i);
+    analogWrite(8, i);
+    analogWrite(7, i);
+  }
+}
+
+void  time_f()
+{
+  dt = millis()-time_last;
+  time_last=millis();
+}
+
+float calc_angle_gyro_f(float angle, float speed)
+{
+  float calculated = 0;  //calculated = Phi_unfiltered;
+  calculated = angle - dt * speed*0.001;
+  return(calculated);
+}
+
+void HPF_gyro_angle_f()
+{
+  HPF_gyro_angle[0] = coefgyro_HPF[0] * HPF_gyro_angle[0] + coefgyro_HPF[1] * calc_angle_gyro_f(Unfilt_angle_gyro[0], gyro[0]) + coefgyro_HPF[2]* calc_angle_gyro_f(Unfilt_angle_gyro_prev[0],gyro_prev[0]);
+  HPF_gyro_angle[1] = coefgyro_HPF[0] * HPF_gyro_angle[1] + coefgyro_HPF[1] * calc_angle_gyro_f(Unfilt_angle_gyro[1], gyro[1]) + coefgyro_HPF[2]* calc_angle_gyro_f(Unfilt_angle_gyro_prev[1],gyro_prev[1]);
+  //HPF_gyro_angle[2] = coefgyro_HPF[0] * HPF_gyro_angle[2] + coefgyro_HPF[1] * gyro[2] + coefgyro_HPF[2]* gyro_prev[2];
+}
+
+void Unfilt_angle_gyro_f()
+{
+  Unfilt_angle_gyro[0] = calc_angle_gyro_f(Unfilt_angle_gyro[0], gyro[0]);
+  Unfilt_angle_gyro[1] = calc_angle_gyro_f(Unfilt_angle_gyro[1], gyro[1]);
+  //Unfilt_angle_gyro[2] = calc_angle_gyro_f(Unfilt_angle_gyro[2], gyro[2]);
+}
+
+void Unfilt_angle_gyro_prev_f()
+{
+  Unfilt_angle_gyro_prev[0] = Unfilt_angle_gyro[0];
+  Unfilt_angle_gyro_prev[1] = Unfilt_angle_gyro[1];
+  //Unfilt_angle_gyro_prev[2] = Unfilt_angle_gyro[2];
+}
+/////////////////////////----------ACCEL PHI-----------------//////////////////////////
+
+float calc_Theta_acc_f(float AccX, float AccY, float AccZ)
+{
+  float calculated;
+  if  (AccZ>0)
+  {
+    calculated = atan(AccX/sqrt(AccY*AccY+AccZ*AccZ))*1/(3.142/180);//degrees roll / phi
+  }
+
+  if  (AccZ<=0)
+  {
+    if (AccX>=0)
+    {
+      calculated = 180-atan(AccX/sqrt(AccY*AccY+AccZ*AccZ))*1/(3.142/180);//degrees pitch / phi
+    }
+    if (AccX<0)
+    {
+      calculated = -180-atan(AccX/sqrt(AccY*AccY+AccZ*AccZ))*1/(3.142/180);//degrees pitch / phi
+    }
+  }
+  return(calculated);
+}
+
+float calc_Phi_acc_f(float AccY, float AccZ)
+{
+  float calculated;
+
+  if  (AccZ>0)
+  {
+    calculated = atan(AccY/AccZ)*1/(3.142/180);//degrees roll / phi
+  }
+  if (AccZ<=0)
+  {
+    if (AccY>=0)
+    {
+      calculated = 180 - atan((AccY)/abs(AccZ))*1/(3.142/180);//degrees roll / phi
+    }
+    if (AccY<0)
+    {
+      calculated = -180 - atan((AccY)/abs(AccZ))*1/(3.142/180);//degrees roll / phi
+    }
+  }
+  return(calculated);
+}
+
+void LPF_acc_angle_f()
+{
+  LPF_acc_angle[0] = coefacc_LPF[0] * LPF_acc_angle[0] + coefacc_LPF[1] * calc_Phi_acc_f(acc[1], acc[2]) + coefacc_LPF[2]* calc_Phi_acc_f(acc_prev[1], acc_prev[2]);
+  LPF_acc_angle[1] = coefacc_LPF[0] * LPF_acc_angle[1] + coefacc_LPF[1] * calc_Theta_acc_f(acc[0], acc[1], acc[2]) + coefacc_LPF[2]* calc_Theta_acc_f(acc_prev[0], acc_prev[1], acc_prev[2]);
+  //LPF_acc_angle[2] = coefacc_LPF[0] * LPF_acc_angle[2] + coefacc_LPF[1] * gyro[2] + coefacc_LPF[2]* gyro_prev[2];
+}
+
+void Complimentary_f()
+{
+  Complimentary[0] = LPF_acc_angle[0] + HPF_gyro_angle[0];
+  Complimentary[1] = LPF_acc_angle[1] + HPF_gyro_angle[1];
+}
+
+//--------------------------------------------------------------
+void readgyro_f()
+{
+  for (i=0; i<3; ++i)
+  {
+    gyro_prev[i]=gyro[i];
+  }
+
+  gyro[0] = myIMU.readFloatGyroX()-gyro_calib[0];
+  gyro[1] = myIMU.readFloatGyroY()-gyro_calib[1];
+  gyro[2] = myIMU.readFloatGyroZ()-gyro_calib[2];
+}
+
+void readacc_f()
+{
+  for (i=0; i<3; ++i)
+  {
+    acc_prev[i]=acc[i];
+  }
+
+  acc[0] = myIMU.readFloatAccelX();
+  acc[1] = myIMU.readFloatAccelY();
+  acc[2] = myIMU.readFloatAccelZ();  
+}
+
+/////////////////--------------------------Z FROM GYRO ----------------------//////
+
+void calc_Z_f(float AccZ)
+{
+  Z_position = Z_position + (AccZ-1)/10*dt*dt;
+}
 
 void setup()
 {
@@ -67,6 +262,16 @@ void setup()
   Serial.begin(9600);
   delay(3000);
 
+  if (myIMU.begin() != 0) {
+    Serial.println("Device error");
+  } 
+
+  Serial.println("Adafruit VL53L0X test");                                    // print text to Serial Monitor
+  if (!lox.begin()) 
+  {                                                         // check if the VL53L0X exist on the I2C adress 0x29
+    Serial.println("Failed to boot VL53L0X");                              // if not exist print text to Serial Monitor
+    while (1);
+  }
 
   //Apply PID gains
   Z_PID.SetTunings(Z.P, Z.I, Z.D);
@@ -78,6 +283,9 @@ void setup()
   Phi_PID.SetMode(Phi_PID.Control::automatic);
   Theta_PID.SetMode(Theta_PID.Control::automatic);
   
+  dt=millis();
+  time_last=millis();
+  /*
   //If the drone is connect to serial, we enter debug mode
   if (Serial)
   {
@@ -110,22 +318,46 @@ void setup()
       }
     }
   }
-
+  */
   //Motors ramp up, to decrease the current spike
   motor_ramp_up();
 }
 
 void loop()
 {
-  while (Z.input > (-ARRAY_SIZE) - 1){
+  
+  while(j < 100){
+    time_f();
+
+    readgyro_f(); //read gyro
+    readacc_f(); // read acc
+    Unfilt_angle_gyro_f(); //calculate gyro angle from unfiltered measurements
+    Unfilt_angle_gyro_prev_f(); // shift unfiltered values
+
+    HPF_gyro_angle_f(); //filter calculated gyro angle
+    LPF_acc_angle_f();  //filter calculated acc angle
+    Complimentary_f();
+
+    lox.rangingTest(&measure, false);                                           // set lox.rangingTest to false
+    Z_actual = measure.RangeMilliMeter*cos((3.142/180)*Complimentary[0])*cos((3.142/180)*Complimentary[1]); // distance to ground in mm
+
+    //PID
+    Z.input = (Z_actual / 100);
+    Phi.input = Complimentary[0] * pi / 180;
+    Theta.input = Complimentary[1] * pi / 180;  
     Phi_PID.Compute();
     Phi.output /= 4 * thrust_const * sin(pi/4) * motor_distance;
     Theta_PID.Compute();
     Theta.output /= 4 * thrust_const * sin(pi/4) * motor_distance;
     Z_PID.Compute(); //After this line Z.output gets updated
-    globalPrefs.output_test[(int)Z.input] = Z.output;
-    Z.output /= cos(Theta.input) * cos(Phi.input);
-    Z.output /= -4 * thrust_const;
+    //Z.output /= cos(Theta.input) * cos(Phi.input);
+    //Z.output /= -4 * thrust_const;
+    Z.output = 0;
+    Serial.print(Z.output);
+    Serial.print(",");
+    Serial.print(Complimentary[0]);
+    Serial.print(",");
+    Serial.println(Complimentary[1]);
     // We calculate angular velocities
     motor_speed[0] = Z.output + Phi.output + Theta.output;
     motor_speed[1] = Z.output - Phi.output + Theta.output;
@@ -152,74 +384,23 @@ void loop()
       }
     }
 
-    globalPrefs.pwm_motor_1[(int)Z.input] = motor_speed[0]; 
-    globalPrefs.pwm_motor_2[(int)Z.input] = motor_speed[1];
-    globalPrefs.pwm_motor_3[(int)Z.input] = motor_speed[2];
-    globalPrefs.pwm_motor_4[(int)Z.input] = motor_speed[3];
+    /*
+    globalPrefs.pwm_motor_1[j] = motor_speed[0]; 
+    globalPrefs.pwm_motor_2[j] = motor_speed[1];
+    globalPrefs.pwm_motor_3[j] = motor_speed[2];
+    globalPrefs.pwm_motor_4[j] = motor_speed[3];
 
-    for (int i = 0; i < 4;i++){
-      analogWrite(10 - i, motor_speed[i]);
+    analogWrite(8, motor_speed[0] * 0.1);
+    analogWrite(7, motor_speed[1] * 0.1);
+    analogWrite(9, motor_speed[2] * 0.1);
+    analogWrite(10, motor_speed[3] * 0.1);
+    */
+
+    //When done send the logs
+    if (j % 10 == 0){
+      myFlashPrefs.writePrefs(&globalPrefs, sizeof(globalPrefs));
+      Serial.println("LOG!");
     }
-    delay(100);
-    Z.input--;
-  }
-  //When done send the logs
-  myFlashPrefs.writePrefs(&globalPrefs, sizeof(globalPrefs));
-  while (1);
-}
-
-// Print preference record to Serial.
-// You have to adapt this to your particular structure.
-void printPreferences(flashPrefs thePrefs)
-{
-  Serial.println("Motor 1: ");
-  for (int i = 0; i < ARRAY_SIZE; i++){
-    Serial.println(thePrefs.pwm_motor_1[i]);
-  }
-  Serial.println("Motor 2: ");
-  for (int i = 0; i < ARRAY_SIZE; i++){
-    Serial.println(thePrefs.pwm_motor_2[i]);
-  }
-  Serial.println("Motor 3: ");
-  for (int i = 0; i < ARRAY_SIZE; i++){
-    Serial.println(thePrefs.pwm_motor_3[i]);
-  }
-  Serial.println("Motor 4: ");
-  for (int i = 0; i < ARRAY_SIZE; i++){
-    Serial.println(thePrefs.pwm_motor_4[i]);
-  }
-  Serial.println("Z: ");
-  for (int i = 0; i < ARRAY_SIZE; i++){
-    Serial.println(thePrefs.output_test[i]);
-  }
-}
-
-// Print return code infos to Serial.
-void printReturnCode(int rc)
-{
-  Serial.print("Return code: ");
-  Serial.print(rc);
-  Serial.print(", ");
-  Serial.println(myFlashPrefs.errorString(rc));
-}
-
-void readPreferenceRecord(flashPrefs globalPrefs){
-  int rc = myFlashPrefs.readPrefs(&globalPrefs, sizeof(globalPrefs));
-  if (rc == FDS_SUCCESS)
-  {
-    printPreferences(globalPrefs);
-  }
-  else
-  {
-    printReturnCode(rc);
-  }
-}
-
-void motor_ramp_up(){
-  for (int i = 0; i < 200;i++){
-    analogWrite(10, i);
-    analogWrite(9, i);
-    analogWrite(8, i);
-    analogWrite(7, i);
+    j++;
   }
 }
